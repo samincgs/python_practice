9a46c633e9028cbc978781c92dc228880460c35d
samin <samincgs@gmail.com>
2024-08-06 15:38:00 -0400

    created a palindrome checker

---
+++ palindrome_checker/main.py
@@ -0,0 +1,27 @@
+
+# racecar level
+def palindrome_checker(word):
+    print('The word is:', word)
+    single_letters = list(word.lower())
+    half = len(single_letters) // 2
+    
+    if len(single_letters) % 2 == 1:
+        a = single_letters[:half]
+        b = single_letters[half + 1:][::-1]
+    else:
+        a = single_letters[:half]
+        b = single_letters[half:][::-1]
+    
+    if a == b:
+        return "It is a palindrome"
+    
+    return "It is a not palindrome"
+    
+
+print(palindrome_checker('Noon'))
+print(palindrome_checker('Racecar'))
+print(palindrome_checker('Level'))
+print(palindrome_checker('Python'))
+print(palindrome_checker('Madam'))
+print(palindrome_checker('Palindrome'))
+
